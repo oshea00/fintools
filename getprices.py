c34b15c74a6fde45adbb3b419dbffbd6920c5e11
Mike OShea <oshea00@gmail.com>
2018-05-09 13:48:12 -0700

    adding delay between prices fetches

--- getprices.py
+++ getprices.py
@@ -1,4 +1,5 @@
 import os
+import time
 from rq import Queue
 from worker import conn
 from stockdb import update_prices
@@ -8,7 +9,9 @@ dburl = os.getenv('DATABASE_URL')
 q = Queue(connection=conn)
 
 q.enqueue(update_prices,'GOOG',dburl)
+time.sleep(5)
 q.enqueue(update_prices,'AAPL',dburl)
+time.sleep(5)
 q.enqueue(update_prices,'FB',dburl)
 
 
