4288f91d4e50f6690a0359e63664bc4ce04afe06
Mike OShea <oshea00@gmail.com>
2018-05-09 14:01:59 -0700

    enhanced delay

--- getprices.py
+++ getprices.py
@@ -9,9 +9,9 @@ dburl = os.getenv('DATABASE_URL')
 q = Queue(connection=conn)
 
 q.enqueue(update_prices,'GOOG',dburl)
-time.sleep(5)
+time.sleep(10)
 q.enqueue(update_prices,'AAPL',dburl)
-time.sleep(5)
+time.sleep(10)
 q.enqueue(update_prices,'FB',dburl)
 
 
