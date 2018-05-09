4b97a532c840e64d354cca9ea90d30f462a04b52
Mike OShea <oshea00@gmail.com>
2018-05-09 11:46:04 -0700

    worker logging?

--- test.py
+++ test.py
@@ -2,10 +2,17 @@ import os
 from rq import Queue
 from worker import conn
 from stockdb import getLast30days
+import logging
+
+log = logging.getLogger()
+log.setLevel(logging.INFO)
+
+log.info("Logging...")
 
 q = Queue(connection=conn)
 
 result = q.enqueue(getLast30days,'GOOG')
 
-print(result)
+log.info(result)
+
 
