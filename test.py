757f97d12fe6fe5431b389a9277abeb35a764edf
Mike OShea <oshea00@gmail.com>
2018-05-09 11:19:47 -0700

    test

--- test.py
+++ test.py
@@ -1,10 +1,11 @@
+import os
 from rq import Queue
 from worker import conn
 from stockdb import getLast30days
 
 q = Queue(connection=conn)
 
-result = q.enqueue(getLast30days('GOOG', 'http://heroku.com'))
+result = q.enqueue(getLast30days,'GOOG')
 
 print(result)
 
