e3446f89dcfa285e940951ec4edce456e570d9b0
Mike OShea <oshea00@gmail.com>
2018-05-09 13:16:00 -0700

    update getprices to get three tickers

--- getprices.py
+++ getprices.py
@@ -8,6 +8,7 @@ dburl = os.getenv('DATABASE_URL')
 q = Queue(connection=conn)
 
 q.enqueue(update_prices,'GOOG',dburl)
+q.enqueue(update_prices,'AAPL',dburl)
+q.enqueue(update_prices,'FB',dburl)
 
-print('Request for GOOG  received')
 
