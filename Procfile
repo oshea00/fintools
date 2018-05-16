09c1e132e42b8d471886be403d82488e7f7ec538
Mike OShea <oshea00@gmail.com>
2018-05-16 09:44:16 -0700

    config file for gunicorn w secure headers

--- Procfile
+++ Procfile
@@ -1,3 +1,2 @@
-web: gunicorn app:app --log-level=info
+web: gunicorn app:app --log-level=info --config config.py
 worker: python -u worker.py
-
---
+++ config.py
@@ -0,0 +1,2 @@
+secure_scheme_headers = {'X-FORWARDED-PROTOCOL': 'ssl', 'X-FORWARDED-PROTO': 'https', 'X-FORWARDED-SSL': 'on'}
+#bind = "0.0.0.0:6000"
